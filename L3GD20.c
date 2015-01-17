c84c66a01af9a38ab4222ee6299797926950391e
Mike Franklin <mike@from-ring-zero.com>
2015-01-17 03:10:11 +0000

    Working on event interface

--- L3GD20.c
+++ L3GD20.c
@@ -27,6 +27,7 @@
 #include <linux/slab.h>
 #include <linux/device.h>
 #include <linux/input.h>
+#include <linux/delay.h>
 #include <asm/uaccess.h>
 
 #include "L3GD20.h"
@@ -45,10 +46,11 @@ static int             count = 0;
 
 #define SAMPLE_COUNT (PAGE_SIZE / sizeof(struct L3GD20_td)) 
 struct L3GD20_device {
-  struct i2c_client *client;
-  int                irq;
-
-  struct device      dev;
+  struct i2c_client   *client;
+  int                  irq;
+  struct device       *dev;
+  struct input_dev    *input;
+  char                 phys[32];
 };
 
 int     L3GD20_open     (struct inode *inode, struct file *filp);
@@ -60,10 +62,15 @@ static int frz_L3GD20_probe (struct i2c_client *client, const struct i2c_device_
 static int frz_L3GD20_remove(struct i2c_client *client);
 static int frz_L3GD20_detect(struct i2c_client *client, struct i2c_board_info *bi);
 
+static int  frz_input_open (struct input_dev *input);
+static void frz_input_close(struct input_dev *input);
+
 static struct i2c_device_id frz_L3GD20_idtable[] = {
   { .name = "L3GD20", .driver_data = 0x0},
   { }
 };
+
+struct L3GD20_d m_data;
 
 static const unsigned short normal_i2c[] = { L3GD20_ADDRESS, I2C_CLIENT_END};
 
@@ -83,6 +90,22 @@ struct i2c_driver frz_L3GD20_driver = {
 
 MODULE_DEVICE_TABLE(i2c, frz_L3GD20_idtable);
 
+static struct attribute *L3GD20_attributes[] = {
+//  &dev_attr_disable.attr,
+//  &dev_attr_calibrate.attr,
+//  &dev_attr_rate.attr,
+//  &dev_attr_autosleep.attr,
+//  &dev_attr_position.attr,
+//#ifdef ADXL_DEBUG
+//  &dev_attr_write.attr,
+//#endif
+  NULL
+};
+
+static const struct attribute_group L3GD20_attr_group = {
+  .attrs = L3GD20_attributes,
+};
+
 /* 
  * Begin utility functions defs
  */
@@ -96,7 +119,7 @@ static int frz_L3GD20_write_byte(struct i2c_client *client, u8 reg, u8 value);
 static irqreturn_t frz_handle_interupt(int irq, void* dev_id) {
   struct L3GD20_d      data;
 
-  struct L3GD20_device *drv;
+  struct L3GD20_device *dev;
   struct i2c_client    *client;
 #ifdef __DEBUG_RATE
   struct timespec t;
@@ -115,23 +138,30 @@ static irqreturn_t frz_handle_interupt(int irq, void* dev_id) {
   count++;
 #endif
 
-  drv = dev_id;
-  client = drv->client;
+  dev = dev_id;
+  client = dev->client;
 
-  if (IS_ERR(client)) return IRQ_NONE;
+  if (client == NULL) {
+    printk("client null\n");
+    return IRQ_NONE;
+  }
+  if (IS_ERR(client)) {
+    printk("client err\n");
+    return IRQ_NONE;
+  }
 
   if (i2c_smbus_read_i2c_block_data(
         client, 
         L3GD20_REG_OUT_X_L | L3GD20_REG_AUTO_INC, 
         sizeof(struct L3GD20_d),  
         (u8*) &data) 
-      == sizeof(struct L3GD20_d)) {
-    // Parse the event
-    printk("%d %d %d\n", data.X, data.Y, data.Z);
-  }
-  else {
-    printk("??\n");
-  }
+      != sizeof(struct L3GD20_d)) return IRQ_NONE;
+
+  input_event(dev->input, EV_ABS, ABS_X, data.X);
+  input_event(dev->input, EV_ABS, ABS_Y, data.Y);
+  input_event(dev->input, EV_ABS, ABS_Z, data.Z);
+
+  //mdelay(30);
 
   return (IRQ_HANDLED);
 }
@@ -141,9 +171,9 @@ static irqreturn_t frz_handle_interupt(int irq, void* dev_id) {
  * @dev_id - should be m_activedev
  */
 static irqreturn_t frz_quick_check(int irq, void* dev_id) {
-  struct L3GD20_device *drv;
+  struct L3GD20_device *dev;
 
-  drv = dev_id;
+  dev = dev_id;
 
   // Get index in buffer, then get time
   //getnstimeofday(&drv->data[drv->cur_index % SAMPLE_COUNT].t);
@@ -157,95 +187,136 @@ static int frz_L3GD20_probe(struct i2c_client *client,
                      const struct i2c_device_id *id) {
   int sig;
   int ret;
+  int err;
 
-  struct L3GD20_device *d = kzalloc(sizeof(*d), GFP_KERNEL);
+  int irq = gpio_to_irq(18);
+
+  struct device *i2c_dev = &client->dev;
+
+  struct L3GD20_device *dev_info = kzalloc(sizeof(*dev_info), GFP_KERNEL);
   struct input_dev *input_dev = input_allocate_device();
+  
+  ret = -EINVAL;
 
-  if (!d || !input_dev) {
+  if (!dev_info || !input_dev) {
     ret = -ENOMEM;
     goto err_free_mem;
   }
 
-  ret = -ENODEV;
-
-  d->client = client;
+  dev_info->input  = input_dev;
+  dev_info->dev    = i2c_dev;
+  dev_info->irq    = irq;
+  dev_info->client = client;
+  
+  input_dev->name = "L3GD20 Gyro";
+  
   sig = frz_L3GD20_read_byte(client, L3GD20_REG_SIG);
   if (sig < 0) {
     printk("Failed to read: %d\n", sig);
-    goto err_free_dev;
+    goto err_free_mem;
   }
 
   if (sig != L3GD20_SIG) {
     printk("Wrong sig returned, got: %X, expected: %X\n", sig, L3GD20_SIG);
-    goto err_free_dev;
+    goto err_free_mem;
   }
 
+  snprintf(dev_info->phys, sizeof(dev_info->phys), "%s/input0", dev_name(i2c_dev));
+
+  input_dev->phys = dev_info->phys;
+  input_dev->dev.parent = i2c_dev;
+  input_dev->id.product = sig;
+  input_dev->id.bustype = BUS_I2C;
+  input_dev->open = frz_input_open;
+  input_dev->close = frz_input_close;
+  
+  input_set_drvdata(input_dev, dev_info);
+
+  __set_bit(EV_ABS, input_dev->evbit);
+  __set_bit(ABS_X, input_dev->absbit);
+  __set_bit(ABS_Y, input_dev->absbit);
+  __set_bit(ABS_Z, input_dev->absbit);
+
+  input_set_abs_params(input_dev, ABS_X, -4096, 4096, 0, 0);
+  input_set_abs_params(input_dev, ABS_Y, -4096, 4096, 0, 0);
+  input_set_abs_params(input_dev, ABS_Z, -4096, 4096, 0, 0);
+
+  __set_bit(EV_KEY, input_dev->evbit);
+   
+  
   // TODO - make this configurable
-  if(frz_L3GD20_write_byte(client, L3GD20_REG_CTRL_REG1, 0b01001111) < 0)    goto err_free_dev;
-  if(frz_L3GD20_write_byte(client, L3GD20_REG_CTRL_REG2, 0b00101001) < 0)    goto err_free_dev;
-  if(frz_L3GD20_write_byte(client, L3GD20_REG_CTRL_REG3, 0b00001000) < 0)    goto err_free_dev;
-  if(frz_L3GD20_write_byte(client, L3GD20_REG_CTRL_REG4, 0b10000000) < 0)    goto err_free_dev;
-  if(frz_L3GD20_write_byte(client, L3GD20_REG_CTRL_REG5, 0b01000000) < 0)    goto err_free_dev;
-  if(frz_L3GD20_write_byte(client, L3GD20_REG_FIFO_CTRL_REG, 0b0100001) < 0) goto err_free_dev;
+  /*
+    dr  = 01
+    bw  = 00
+    PD  = 1
+    Zen = 1
+    Xen = 1
+    Yen = 1
+  */
+  if(frz_L3GD20_write_byte(client, L3GD20_REG_CTRL_REG1, 0b01001111) < 0)    goto err_free_mem;
+  if(frz_L3GD20_write_byte(client, L3GD20_REG_CTRL_REG2, 0b00101001) < 0)    goto err_free_mem;
+  if(frz_L3GD20_write_byte(client, L3GD20_REG_CTRL_REG3, 0b00001000) < 0)    goto err_free_mem;
+  if(frz_L3GD20_write_byte(client, L3GD20_REG_CTRL_REG4, 0b10000000) < 0)    goto err_free_mem;
+  if(frz_L3GD20_write_byte(client, L3GD20_REG_CTRL_REG5, 0b01000000) < 0)    goto err_free_mem;
+  if(frz_L3GD20_write_byte(client, L3GD20_REG_FIFO_CTRL_REG, 0b0100001) < 0) goto err_free_mem;
+
+  err = sysfs_create_group(&i2c_dev->kobj, &L3GD20_attr_group);
+  if (err)
+    goto err_free_mem;
+
+  err = input_register_device(input_dev);
+  if (err)
+    goto err_remove_attr;
+
+  i2c_set_clientdata(client, dev_info);
 
   // TODO - not make this hard coded
-  d->irq = gpio_to_irq(18);
-  printk("IRQ %d\n", d->irq);
-  if ((ret = request_threaded_irq(d->irq,
+  printk("IRQ %d\n", dev_info->irq);
+  if ((ret = request_threaded_irq(dev_info->irq,
                            frz_quick_check, 
                            frz_handle_interupt, 
                            IRQF_ONESHOT | IRQF_TRIGGER_RISING | IRQF_TRIGGER_HIGH  , 
                            "L3GD20_rising_high", 
-                           d)) < 0) {
-    goto err_free_dev;
+                           dev_info)) < 0) {
+    goto err_free_mem;
   }
-
-  device_initialize(&d->dev);
-  d->dev.class = L3GD20_class;
-  d->dev.parent = &client->dev;
-  d->dev.groups = NULL;
-  d->dev.release = NULL;
-  ret = kobject_set_name(&d->dev.kobj, "L3GD20-%d", MINOR(d->dev.devt));
-  if (ret < 0) {
-    printk("Failed to set name %d\n", ret);
-    goto err_free_irq;
-  }
-
-  ret = device_add(&d->dev);
-  if (ret < 0) {
-    printk("Failed to add device %d\n", ret);
-    goto err_free_irq;
-  }
-  i2c_set_clientdata(client, d);
 
   printk("Success\n");
   return 0;
 
-//err_rem_device:
-//  device_del(&d->dev);
-err_free_irq:
-  free_irq(d->irq, d);
-err_free_dev:
-  d->client = NULL;
+//err_free_irq:
+//  free_irq(dev_info->irq, dev_info);
+err_remove_attr:
+  sysfs_remove_group(&i2c_dev->kobj, &L3GD20_attr_group);
 err_free_mem:
-  kfree(d);
+  input_free_device(input_dev);
+  kfree(dev_info);
   return ret;
 }
 
+static int frz_input_open(struct input_dev *input) {
+  return 0; 
+}
+
+static void frz_input_close(struct input_dev *input) {
+
+}
+
 static int frz_L3GD20_remove(struct i2c_client *client) {
-  struct L3GD20_device *dv;
+  struct L3GD20_device *dev;
 
   printk("Removing \n");
 
-  dv = i2c_get_clientdata(client);
-  if (!dv) {
+  dev = i2c_get_clientdata(client);
+  if (!dev) {
     printk("WTF?? %s\n", __func__);
     return -ENODEV;
   }
 
-  free_irq(dv->irq, dv); dv->client = NULL;
-
-  device_del(&dv->dev);
+  sysfs_remove_group(&dev->dev->kobj, &L3GD20_attr_group);
+  free_irq(dev->irq, dev); dev->client = NULL;
+  input_unregister_device(dev->input);
+  kfree(dev);
 
   return 0;
 }
